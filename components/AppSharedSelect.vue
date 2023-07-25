1e1f31272c204fecb6fbe8d515b3d11fb75c702e
Liv Heller <lheller@bitwiseindustries.com>
2023-07-24 19:30:58 -0600

    feat(SharedSelect): Create shared select component to be used for filtering through dogs.

---
+++ components/AppSharedSelect.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="selectcontainer">
+<label :for="select">Filter by {{ select }}</label>
+<select @change="$emit('change', $event.target.value)" :id="select" name="select">
+<option value> -- Please Select a {{ select }}</option>
+<option v-for="option in selectOptions" :key="option" :value="option">{{ option }}</option>
+
+</select>
+    </div>
+</template>
+
+<script>
+    export default {
+    props: {
+            select: {
+            type: String,
+                default: "Service"
+            },
+            selectOptions: {
+                 type: Array,
+                default: () => ["Bath", "Walk", "Hike", "Playtime", "Teeth Cleaning", "Get Vaccine"]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+label {
+    font-size: 1.5rem;
+    margin-right: 1rem
+}
+select {
+    width: 60%;
+    padding: 3px;
+}
+.selectcontainer {
+   display: flex
+}
+
+</style>
--- pages/dogs.vue
+++ pages/dogs.vue
@@ -1,22 +1,50 @@
 <template>
   <main class="container">
-    <h1>The Doggos</h1>
-    <AppDogInfo :datasource="dogdata" />
+    <div class="dogheading">
+      <h1>The Doggos</h1>
+      <hr />
+      <section class="filtersContainer">
+        <AppSharedSelect @change="selectedService = $event" />
+        <AppSharedSelect @change="selectedStatus = $event" select="Status" :selectOptions="boardingFilterOptions" />
+      </section>
+    </div>
+    <AppDogInfo :datasource="filteredDogs" />
   </main>
 </template>
 
 <script>
 import AppDogInfo from "@/components/AppDogInfo.vue";
+import AppSharedSelect from '@/components/AppSharedSelect.vue';
 import { mapState } from "vuex";
 
 export default {
   computed: {
     ...mapState(["dogdata"]),
+    filteredDogs() {
+      if (this.selectedService) {
+        return this.dogdata.filter(el => {
+          let services = el.services;
+          return services.includes(this.selectedService)
+            })
+        
+      }
+      return this.dogdata
+    },
+    boardingFilterOptions() {
+      return ["Boarding", "Daycare", "Not Checked-In"]
+    }
   },
 
   components: {
     AppDogInfo,
+    AppSharedSelect 
   },
+
+  data() {
+    return {
+      selectedService: "",
+    }
+  }
 };
 </script>
 
@@ -26,5 +54,17 @@ h1 {
   font-size: 2.5rem;
   font-weight: 600;
   padding-top: 3rem;
+}
+hr {
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2rem;
+  opacity: .5;
+}
+.filtersContainer {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 1rem;
 }
 </style>
