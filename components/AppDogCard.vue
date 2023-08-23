783df6bc3789502ed98a7dd132619b66cb0e1dc3
Liv Heller <lheller@bitwiseindustries.com>
2023-08-23 13:40:26 -0600

    feat(Modal): Additional styling of icons. Pointer set to default. Icons given conditionals.

--- components/AppDogCard.vue
+++ components/AppDogCard.vue
@@ -17,6 +17,7 @@
       </div>
 
       <div class="card-dog-info">
+        <!-- @TODO Create shared function to auto capitalize first letters of words -->
         <p><b>Breed:</b> {{ data.breed }}</p>
         <p><b>Sex:</b> {{ data.gender }}</p>
         <p><b>Age:</b> {{ data.age }}</p>
@@ -26,14 +27,39 @@
             >Go To Calendar
           </nuxt-link>
         </h2>
+        <!-- @TODO Make these dynamic and clickable -->
         <div class="icon-container">
-          <fa icon="bath" class="icon" />
-          <fa icon="utensils" class="icon" />
-          <fa icon="syringe" class="icon" />
-          <fa icon="tooth" class="icon" />
-          <fa icon="shoe-prints" class="icon" />
-          <fa icon="hiking" class="icon" />
-          <fa icon="baseball-ball" class="icon" />
+          <fa v-if="data.services?.includes('Bath')" icon="bath" class="icon" />
+          <fa
+            v-if="data.services?.includes('Meal')"
+            icon="utensils"
+            class="icon"
+          />
+          <fa
+            v-if="data.services?.includes('Get Vaccine')"
+            icon="syringe"
+            class="icon"
+          />
+          <fa
+            v-if="data.services?.includes('Teeth Cleaning')"
+            icon="tooth"
+            class="icon"
+          />
+          <fa
+            v-if="data.services?.includes('Walk')"
+            icon="shoe-prints"
+            class="icon"
+          />
+          <fa
+            v-if="data.services?.includes('Hike')"
+            icon="hiking"
+            class="icon"
+          />
+          <fa
+            v-if="data.services?.includes('Playtime')"
+            icon="baseball-ball"
+            class="icon"
+          />
         </div>
       </div>
       <div class="card-owner-container">
@@ -43,6 +69,7 @@
           v-for="detail in data.owner"
         >
           <h2>{{ detail.ownerName }}</h2>
+          <hr />
           <!-- @TODO link email to pull up native email -->
           <p><fa icon="envelope" class="icon" /> {{ detail.email }}</p>
           <p><fa icon="phone" class="icon" /> {{ detail.phone }}</p>
@@ -56,6 +83,8 @@
 </template>
 
 <script>
+import { CapitalizeFirstLetter } from "../util/CapitalizeFirstLetter";
+
 export default {
   name: "Card",
   props: {
@@ -89,6 +118,7 @@ hr {
 .timestamp {
   margin-left: 1rem;
   margin-top: 0.5rem;
+  cursor: default;
 }
 .card-body {
   display: grid;
@@ -99,6 +129,7 @@ hr {
 }
 .label {
   position: relative;
+  cursor: default;
 
   .icon {
     display: inline-block;
@@ -126,6 +157,8 @@ hr {
 .card-dog-info {
   grid-column: 2;
   margin-left: 1rem;
+  cursor: default;
+
   p {
     font-weight: 400;
   }
@@ -139,6 +172,7 @@ hr {
 }
 .card-owner-container {
   text-align: center;
+  cursor: default;
 }
 .card-dog-owner {
   border: 1px solid (rgb(55, 46, 41, 0.2));
@@ -156,11 +190,19 @@ hr {
       margin-top: 0.4rem;
     }
   }
+  hr {
+    width: 75%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 1rem;
+    color: black;
+  }
 }
 h1 {
   font-size: 3rem;
   padding: 0rem 1rem;
   text-align: center;
+  cursor: default;
 }
 h2 {
   font-weight: 500;
@@ -189,7 +231,7 @@ img {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   row-gap: 1rem;
-  column-gap: 2rem;
+  column-gap: 1rem;
   padding-left: 1rem;
   padding-right: 1rem;
   height: 50%;
